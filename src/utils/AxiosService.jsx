abe162f1837a23749e8a2942055306c4f9e9f36b
PRIYA <148181155+priyarajesh7696@users.noreply.github.com>
2024-09-16 17:07:01 +0530

    Update AxiosService.jsx

--- src/utils/AxiosService.jsx
+++ src/utils/AxiosService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const AxiosService = axios.create({
-    baseURL:"http://localhost:8000",
+    baseURL:"http://192.168.1.3:8000",
     headers:{
         "Content-Type":"application/json",
     }
