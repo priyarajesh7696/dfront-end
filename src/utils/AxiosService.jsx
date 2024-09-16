9a423b55a4a192b7898626246563875d5fac01f5
PRIYA <148181155+priyarajesh7696@users.noreply.github.com>
2024-09-16 17:52:02 +0530

    Update AxiosService.jsx

--- src/utils/AxiosService.jsx
+++ src/utils/AxiosService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const AxiosService = axios.create({
-    baseURL:"http://192.168.1.3:8000",
+    baseURL:"https://dback-end.vercel.app/",
     headers:{
         "Content-Type":"application/json",
     }
